883168853d3671aa7ce668e60168b699419ff665
Cristopher Laluna <153648185+Laluna-Cristopher@users.noreply.github.com>
2024-10-02 23:00:49 +0800

    Create Mathtest.java

---
+++ chapter 4 /Mathtest.java
@@ -0,0 +1,17 @@
+class MathTest{
+   public static void main(String []args){
+   
+   System.out.println("SQRT: "  + Math.sqrt(37.5));
+   System.out.println("SIN: "   + Math.sin(37.5));
+   System.out.println("COS: "   + Math.cos(37.5));
+   System.out.println("FLOOR: " + Math.floor(22.0));
+   System.out.println("CEIL: "  + Math.ceil(23.0));
+   System.out.println("ROUND: " + Math.round(23));
+   System.out.println("MAX: "   + Math.max(71,68));
+   System.out.println("MIN: "   + Math.min(71,68));
+   System.out.println("RANDOM: "+ Math.random());
+
+   
+   }
+
+}
