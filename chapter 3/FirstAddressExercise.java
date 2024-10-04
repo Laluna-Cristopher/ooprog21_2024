a78feccee93c092fd8c47eccefe69db6f5333483
Cristopher Laluna <153648185+Laluna-Cristopher@users.noreply.github.com>
2024-10-04 08:45:24 +0700

    Add files via upload

---
+++ chapter 3/FirstAddressExercise.java
@@ -0,0 +1,13 @@
+public class FirstAddressExercise {
+
+public static void main(String[] args) {
+
+String brand = "Apple";
+
+String address = "1 Apple Park Way, Cupertino, CA 95014, USA";
+
+System.out.println("Headquarters Address of " + brand + ": " + address);
+
+}
+
+}
